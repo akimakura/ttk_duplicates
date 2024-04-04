d4ab6b4c3674822d6b3a8ab821f1dca122ffabbb
Akim <akimik2002@gmail.com>
2024-04-05 00:43:58 +0300

    Теперь данные записываются в один столбец, а не два файла

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ import pandas as pd
 d = {"Резерв": "ОПР",
      "ОПР": "Резерв"}
 
-# d = {"Лист2": "Лист3",
-#      "Лист3": "Лист2"}
-
 all_results = []
+
+name_exl_1 = input("Напишите название первого файла, по примеру *** exl/имя_файла.xls *** = ")
+name_exl_2 = input("Напишите название второго файла, по примеру *** exl/имя_файла.xls *** = ")
 
 for i in d.keys():
 
-    open_one = pd.read_excel('exl/Проба 1.xls', engine='xlrd', sheet_name=d[i])
+    open_one = pd.read_excel(name_exl_1, sheet_name=d[i])
     one_data = open_one.iloc[:, 4]
-    open_two = pd.read_excel('exl/Проба 2.xls', engine='xlrd', sheet_name=i)
+    open_two = pd.read_excel(name_exl_2, sheet_name=i)
     two_data = open_two.iloc[:, 4]
 
     # превращаем наши данные в массив numpy
@@ -26,11 +26,10 @@ for i in d.keys():
 
     # находим пересечения в двух массивах
     result = np.intersect1d(one_arr, two_arr)
-    print(result)
     all_results.extend(result)
 
 
 # создаем excel файл
 data_frame = pd.DataFrame(all_results, columns=['При одновременных авариях по обоим напрвлениям пострадают заказы:'])
-excel_file = 'exl/new_xl.xlsx'
+excel_file = 'exl/по_обоим_напрвлениям_пострадают.xlsx'
 data_frame.to_excel(excel_file, index=False)
